776c7210508ae50b4682fb492e4a7324c6bb5700
Etienne Giroux <etienneg@Etiennes-MacBook-Pro.local>
2020-07-27 19:40:16 -0400

    Start from M4-4 Solution

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,103 +2,147 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-import useInterval from "../hooks/use-interval.hook";
-
-import cookieSrc from "../cookie.svg";
+import cookieSrc, { ReactComponent } from "../cookie.svg";
 import Item from "./Item";
 
+import useInterval from "../hooks/use-interval.hook";
+import useKeydown from "../hooks/use-keydown.hook";
+import useDocumentTitle from "../hooks/use-documentTitle.hook";
+
 const items = [
-  { id: "cursor", name: "Cursor", cost: 10, value: 1 },
-  { id: "grandma", name: "Grandma", cost: 100, value: 10 },
-  { id: "farm", name: "Farm", cost: 1000, value: 80 },
+  {
+    id: "cursor",
+    name: "Cursor",
+    type: "cps",
+    basePrice: 10,
+    cost: 10,
+    value: 1,
+
+    growth: 1.2,
+  },
+  {
+    id: "grandma",
+    name: "Grandma",
+    type: "cps",
+    basePrice: 100,
+    cost: 100,
+    value: 10,
+    growth: 1.15,
+  },
+  {
+    id: "farm",
+    name: "Farm",
+    type: "cps",
+    basePrice: 1000,
+    cost: 1000,
+    value: 80,
+    growth: 1.12,
+  },
+  {
+    id: "megaCursor",
+    name: "Mega Cursor",
+    type: "cursor",
+    basePrice: 50,
+    cost: 50,
+    value: 0,
+    growth: 1.3,
+  },
 ];
 
-const calculateCookiesPerSecond = (purchasedItems) => {
-  return Object.keys(purchasedItems).reduce((acc, itemId) => {
-    const numOwned = purchasedItems[itemId];
-    const item = items.find((item) => item.id === itemId);
-    const value = item.value;
-
-    return acc + value * numOwned;
-  }, 0);
-};
-
 const Game = () => {
-  const [numCookies, setNumCookies] = React.useState(1000);
-
+  const [numCookies, setNumCookies] = React.useState(0);
   const [purchasedItems, setPurchasedItems] = React.useState({
     cursor: 0,
     grandma: 0,
     farm: 0,
+    megaCursor: 0,
   });
 
-  const incrementCookies = () => {
-    setNumCookies((c) => c + 1);
+  const cookiesPerClick = purchasedItems.megaCursor + 1;
+
+  const addCookie = () => {
+    setNumCookies(numCookies + cookiesPerClick);
+  };
+
+  const setPrices = (array) => {
+    array.forEach((item) => {
+      let cost = item.basePrice * item.growth ** purchasedItems[item.id];
+
+      const itemToUpdatePriceFor = items.find((entry) => entry.id === item.id);
+
+      itemToUpdatePriceFor.cost = Math.floor(cost);
+    });
+  };
+
+  setPrices(items);
+
+  useDocumentTitle(`${numCookies} cookies - Cookie Clicker`, `Cookie Clicker`);
+
+  useKeydown("Space", addCookie);
+
+  const purchaseItem = (item) => {
+    if (numCookies < item.cost) {
+      return alert("Can't afford this item");
+    } else {
+      setNumCookies(numCookies - item.cost);
+      setPurchasedItems({
+        ...purchasedItems,
+        [item.id]: purchasedItems[item.id] + 1,
+      });
+      calculateCookiesPerSec(purchasedItems);
+    }
+  };
+
+  const calculateCookiesPerSec = (purchasedItems) => {
+    let numOfGeneratedCookies = 0;
+
+    items.forEach((item) => {
+      numOfGeneratedCookies =
+        numOfGeneratedCookies + purchasedItems[item.id] * item.value;
+    });
+
+    return numOfGeneratedCookies;
   };
 
   useInterval(() => {
-    const numOfGeneratedCookies = calculateCookiesPerSecond(purchasedItems);
-
-    setNumCookies(numCookies + numOfGeneratedCookies);
+    const cookiesPerSec = calculateCookiesPerSec(purchasedItems);
+    setNumCookies(numCookies + cookiesPerSec);
   }, 1000);
-
-  React.useEffect(() => {
-    document.title = `${numCookies} cookies - Cookie Clicker Workshop`;
-
-    return () => {
-      document.title = "Cookie Clicker Workshop";
-    };
-  }, [numCookies]);
-
-  React.useEffect(() => {
-    const handleKeydown = (ev) => {
-      if (ev.code === "Space") {
-        incrementCookies();
-      }
-    };
-
-    window.addEventListener("keydown", handleKeydown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeydown);
-    };
-  });
 
   return (
     <Wrapper>
       <GameArea>
         <Indicator>
           <Total>{numCookies} cookies</Total>
-          <strong>{calculateCookiesPerSecond(purchasedItems)}</strong> cookies
-          per second
+          <strong>{calculateCookiesPerSec(purchasedItems)}</strong>{" "}
+          {calculateCookiesPerSec(purchasedItems) === 1
+            ? "cookie per second"
+            : "cookies per second"}
+          <div>
+            <strong>{cookiesPerClick}</strong>{" "}
+            {cookiesPerClick === 1 ? "cookie per click" : "cookies per click"}
+          </div>
         </Indicator>
-        <Button onClick={incrementCookies}>
-          <Cookie src={cookieSrc} />
+        <Button>
+          <Cookie src={cookieSrc} onClick={addCookie} />
         </Button>
       </GameArea>
 
       <ItemArea>
         <SectionTitle>Items:</SectionTitle>
-        {items.map((item, index) => {
+        {items.map((entry, index) => {
+          let firstItem = false;
+          if (index === 0) {
+            firstItem = true;
+          }
           return (
             <Item
-              key={item.id}
-              index={index}
-              name={item.name}
-              cost={item.cost}
-              value={item.value}
-              numOwned={purchasedItems[item.id]}
-              handleAttemptedPurchase={() => {
-                if (numCookies < item.cost) {
-                  alert("Cannot afford item");
-                  return;
-                }
-
-                setNumCookies(numCookies - item.cost);
-                setPurchasedItems({
-                  ...purchasedItems,
-                  [item.id]: purchasedItems[item.id] + 1,
-                });
+              key={entry.id}
+              item={entry}
+              numOwned={purchasedItems[entry.id]}
+              focusOnLoad={firstItem}
+              handleClick={() => {
+                purchaseItem(entry);
               }}
             />
           );
@@ -122,11 +166,6 @@ const Button = styled.button`
   border: none;
   background: transparent;
   cursor: pointer;
-  transform-origin: center center;
-
-  &:active {
-    transform: scale(0.9);
-  }
 `;
 
 const Cookie = styled.img`
