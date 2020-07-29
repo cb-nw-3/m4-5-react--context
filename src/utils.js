990a759601aec90a0a043c79ac5dbaed9ba047eb
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-07-28 22:42:54 -0400

    ex2 removed fx from utils

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
-//this function requires the state of purchastedItems and the { items } data
-export const calculateCookiesPerSecond = (purchasedItems, gameItems) => {
-  return Object.keys(purchasedItems).reduce((acc, itemId) => {
-    const numOwned = purchasedItems[itemId];
-    const item = gameItems.find((item) => item.id === itemId);
-    const value = item.value;
+// //this function requires the state of purchastedItems and the { items } data
+// export const calculateCookiesPerSecond = (purchasedItems) => {
+//   return Object.keys(purchasedItems).reduce((acc, itemId) => {
+//     const numOwned = purchasedItems[itemId];
+//     const item = items.find((item) => item.id === itemId);
+//     const value = item.value;
 
-    return acc + value * numOwned;
-  }, 0);
-};
+//     return acc + value * numOwned;
+//   }, 0);
+// };
