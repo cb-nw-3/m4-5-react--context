ac066a4ee8b365cace34ba369c3646303910f536
Mae <maeberris@outlook.com>
2020-07-29 20:55:13 -0400

    solved problem 4

--- src/components/GameContext.js
+++ src/components/GameContext.js
@@ -18,16 +18,38 @@ export const GameProvider = ({ children }) => {
   const [cookiesPerClick, setCookiesPerClick] = React.useState(1);
   const [items, setItems] = useStickyState(itemsArray, "items");
 
-  const calculateCookiesPerTick = (listOfItems) => {
+  const calculateCookiesPerTick = () => {
     let total = 0;
     const filteredItems = items.filter((item) => {
       return item.type === "tick";
     });
     filteredItems.forEach((item) => {
-      total += listOfItems[`${item.id}`] * item.value;
+      total += purchasedItems[`${item.id}`] * item.value;
     });
     return total;
   };
+
+  React.useEffect(() => {
+    const newDate = new Date();
+    const newDateInMili = newDate.getTime();
+
+    const oldMiliDateJSON = window.localStorage.getItem("dateInMili");
+    if (oldMiliDateJSON !== null) {
+      const oldMiliDate = JSON.parse(oldMiliDateJSON);
+      const miliDifference = newDateInMili - oldMiliDate;
+      console.log(miliDifference);
+      const numOfTicks = Math.floor(miliDifference / 1000);
+      if (numOfTicks > 0) {
+        setNumCookies(numCookies + calculateCookiesPerTick() * numOfTicks);
+      }
+    }
+  }, []);
+
+  React.useEffect(() => {
+    const date = new Date();
+    const dateInMili = date.getTime();
+    window.localStorage.setItem("dateInMili", JSON.stringify(dateInMili));
+  });
 
   return (
     <GameContext.Provider
