0fa4bf30236d9605c452c5192e614bdaf84185fd
jenny ma <jenny_lau_8@hotmail.com>
2020-08-01 18:53:26 -0400

    added .data.js and import in App and Game

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
 import Home from "./Home";
 import Game from "./Game";
+import items from "../data.js";
 
 function App(props) {
   return (
--- src/components/Game.js
+++ src/components/Game.js
@@ -6,12 +6,7 @@ import useInterval from "../hooks/use-interval.hook";
 
 import cookieSrc from "../cookie.svg";
 import Item from "./Item";
-
-const items = [
-  { id: "cursor", name: "Cursor", cost: 10, value: 1 },
-  { id: "grandma", name: "Grandma", cost: 100, value: 10 },
-  { id: "farm", name: "Farm", cost: 1000, value: 80 },
-];
+import items from "../data.js";
 
 const calculateCookiesPerSecond = (purchasedItems) => {
   return Object.keys(purchasedItems).reduce((acc, itemId) => {
