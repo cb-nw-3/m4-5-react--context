bbba25dd41e763f85925a4305d334c287f45ab00
Roger Lam <rogerlam@wylco.org>
2020-08-01 12:26:05 -0400

    finish ex 1

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,17 +11,6 @@ import items from "../data";
 
 const Game = ({ numCookies, setNumCookies, purchasedItems, setPurchasedItems }) => {
 
-  // ok. let's think this through.
-  // essentially, all the data that we have is supposed to go into localStorage
-  // so that it persists through tab closures, what-have-you, like the real cookie clicker
-  // it needs to restore the cookies upon first load, but only once per session
-  // SO! Can we check to see if the session exists? And if it exists, we load.
-
-  if (!sessionStorage.getItem("cookieFirstLoad")) {
-    sessionStorage.setItem("cookieFirstLoad", true);
-    setNumCookies(localStorage.getItem("numCookies"));
-  }
-
   const calculateCookiesPerSecond = (purchasedItems) => {
     return Object.keys(purchasedItems).reduce((acc, itemId) => {
       const numOwned = purchasedItems[itemId];
@@ -32,6 +21,34 @@ const Game = ({ numCookies, setNumCookies, purchasedItems, setPurchasedItems }) 
     }, 0);
   };
 
+  // ok. let's think this through.
+  // essentially, all the data that we have is supposed to go into localStorage
+  // so that it persists through tab closures, what-have-you, like the real cookie clicker
+  // it needs to restore the cookies upon first load, but only once per session
+  // SO! Can we check to see if the session exists? And if it exists, we load.
+
+  if (!sessionStorage.getItem("cookieFirstLoad")) {
+    sessionStorage.setItem("cookieFirstLoad", true);
+
+    setNumCookies(Number(localStorage.getItem("numCookies")));
+  }
+
+  // second problem: we need to add the cookies we earned while off-page
+  // suppose we could just do another logic check for date, and if it's less than a second, we skip
+  // if we just put this in the main body, it re-renders a ton. we shold not do that.
+
+  // put this in a useEffect with an empty dependency array
+  // it will render once, but then since it's empty, it doesn't 'watch' anything
+
+  React.useEffect(() => {
+
+    let timeElapsed = Math.floor((new Date().getTime() - localStorage.getItem("lastUpdateTime")) / 1000);
+
+    if (timeElapsed > 1) {
+      setNumCookies(Number(localStorage.getItem("numCookies")) + Number(timeElapsed * calculateCookiesPerSecond(purchasedItems)));
+    }
+  }, [])
+
   const incrementCookies = () => {
     setNumCookies((c) => c + 1);
   };
@@ -41,6 +58,8 @@ const Game = ({ numCookies, setNumCookies, purchasedItems, setPurchasedItems }) 
 
     setNumCookies(numCookies + numOfGeneratedCookies);
     localStorage.setItem("numCookies", numCookies);
+    let lastUpdateTime = new Date();
+    localStorage.setItem("lastUpdateTime", lastUpdateTime.getTime());
   }, 1000);
 
   React.useEffect(() => {
