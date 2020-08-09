3ab0e572b64dba00b691da5bbcba0a309472864e
Nathalie Benarroch <nathaliebenarroch@Nathalies-MacBook-Pro.local>
2020-08-09 10:04:27 -0400

    Completed with useContext, useState and useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import GlobalStyles from "./GlobalStyles";
 import Home from "./Home";
 import Game from "./Game";
 
-function App(props) {
+function App() {
   return (
     <>
       <GlobalStyles />
